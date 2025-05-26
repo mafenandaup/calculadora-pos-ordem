fffb2f04db64026a078b3f38b57c78b6d59bbe42
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2025-05-26 09:33:16 -0300

    create project

---
+++ src/main/java/com/example/calc_pos_ordem/CalcPosOrdemApplication.java
@@ -0,0 +1,13 @@
+package com.example.calc_pos_ordem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcPosOrdemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcPosOrdemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/calc_pos_ordem/CalcPosOrdemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calc_pos_ordem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcPosOrdemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
